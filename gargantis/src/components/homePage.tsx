6ce97d241bd37c6c152bd71ece5896ef2ab96b09
Murat Barış Erol <58422636+bariserol761@users.noreply.github.com>
2025-01-13 21:16:48 +0300

    refactor: update HomePage layout and styles, enhance visual elements

--- gargantis/src/components/homePage.tsx
+++ gargantis/src/components/homePage.tsx
@@ -10,11 +10,11 @@ export const HomePage = () => {
       <div style={{ marginTop: "7rem" }}></div>
 
       {/* Content Section */}
-      <div className="grid" style={{ height: "80vh" }}>
+      <div className="grid" style={{ height: "100vh" }}>
         {/* Text Section */}
         <div className="col ml-5 mr-2">
           <div className="font-bold text-8xl">
-            Grafik Tasarım ile Markanızı Güçlendirin
+            Garganist Tasarım ile Markanızı Güçlendirin
           </div>
           <div className="font-bold text-3xl mt-5">
             Grafik tasarım hizmetlerimizle markanızın hikayesini yaratıcı ve
@@ -26,14 +26,51 @@ export const HomePage = () => {
         </div>
 
         {/* Image Section */}
-        <div className="col-6 bg-blue-400 mr-5 ml-2">
-          <img
-            src={tasarim1}
-            alt="hero"
-            className="w-full h-full object-cover"
-          />
+        <div className="col-6 mr-5 ml-2 flex justify-content-center align-items-center">
+          <img src={tasarim1} alt="hero" className="w-10 h-10 object-cover" />
         </div>
       </div>
+      <div className="flex text-7xl font-bold mt-8 align-items-center justify-content-center">
+        Grafik Tasarım ile Fark Yaratın
+      </div>
+      <div
+        className="col-6 mr-5 ml-2 flex justify-content-center align-items-center"
+        style={{
+          height: "100%",
+          background:
+            "linear-gradient(to right, #a5ddea, #812e8b, #dc4699, #59499e)",
+          WebkitBackgroundClip: "text",
+          WebkitTextFillColor: "transparent",
+          backgroundSize: "400% 100%", // Large gradient to enable smooth transition
+          animation: "gradientShift 6s linear ", // Smooth loop animation
+          fontSize: "2rem",
+          fontWeight: "bold",
+        }}
+      >
+        This is the text
+      </div>
+
+      <style>
+        {`
+    @keyframes gradientShift {
+      0% {
+        background-position: 0% 50%; /* Start with the first color (light blue) */
+      }
+      25% {
+        background-position: 25% 50%; /* Transition to the second color (purple) */
+      }
+      50% {
+        background-position: 50% 50%; /* Transition to the third color (pink) */
+      }
+      75% {
+        background-position: 75% 50%; /* Transition to the fourth color (dark blue) */
+      }
+      100% {
+        background-position: 100% 50%; /* Transition back to the first color (light blue) */
+      }
+    }
+  `}
+      </style>
     </div>
   );
 };
